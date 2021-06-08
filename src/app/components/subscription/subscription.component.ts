ff2a3892cd4fcb52c0f65edf9131c61865b026b3
Linus <vitocorleone77@gmail.com>
2021-06-08 22:12:43 +0200

    version strict

--- src/app/components/subscription/subscription.component.ts
+++ src/app/components/subscription/subscription.component.ts
@@ -1,6 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl } from '@angular/forms'
+import { debounceTime } from 'rxjs/operators';
+
 import { Customer } from './Customer';
+
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if (emailControl?.pristine || confirmControl?.pristine) {
+    return null;
+  }
+
+  if (emailControl?.value === confirmControl?.value) {
+    return null;
+  }
+  return { match: true };
+}
+
+// since an object key can be any of those types, our key can too
+// in TS 3.0+, putting just "string" raises an error
+function hasKey<O>(obj: O, key: PropertyKey): key is keyof O {
+  return key in obj
+}
+
 @Component({
   selector: 'app-subscription',
   templateUrl: './subscription.component.html',
@@ -10,16 +33,33 @@ export class SubscriptionComponent implements OnInit {
 
   signupForm!: FormGroup;
   customer = new Customer();
+  emailMessage: string = '';
 
-  constructor(private formBuilder: FormBuilder) { }
+  private validationMessages = {
+    required: 'Please enter your email address.',
+    email: 'Please enter a valid email address.'
+  };
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.signupForm = this.formBuilder.group({
-      firstName: ['', Validators.required, Validators.minLength(3)],
-      lastName: ['', Validators.required, Validators.maxLength(50)],
-      email: ['', Validators.required, Validators.email ],
-      sendCatalog: true
+    this.signupForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', Validators.required],
+      }, { validator: emailMatcher }),
+      phone: '',
+      addresses: this.fb.array([this.buildAddress()])
     });
+    const emailControl = this.signupForm.get('emailGroup.email');
+
+    emailControl?.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(
+      value => this.setMessage(emailControl)
+    );
   }
 
   save(): void {
@@ -27,4 +67,26 @@ export class SubscriptionComponent implements OnInit {
     console.log('Saved: ' + JSON.stringify(this.signupForm.value));
   }
 
+  buildAddress(): FormGroup {
+    return this.fb.group({
+      addressType: 'home',
+      street1: ['', Validators.required],
+      street2: '',
+      city: '',
+      state: '',
+      zip: ''
+    });
+  }
+
+  setMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this.emailMessage = Object.keys(c.errors).map(
+        key => {
+          if (hasKey(this.validationMessages, key)) {
+            this.validationMessages[key]
+          }
+        }).join(' '); // works fine!
+    }
+  }
 }
