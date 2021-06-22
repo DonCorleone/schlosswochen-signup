e98decc65f3da28a590707ec7593b2bd06ac90f7
Linus <vitocorleone77@gmail.com>
2021-06-22 22:27:50 +0200

    ngrx removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,6 @@ import { APOLLO_OPTIONS } from 'apollo-angular';
 import * as realm from './realm';
 import { GraphQLModule } from './graphql.module';
 import { AppRoutingModule } from './app-routing.module';
-import { StoreModule } from '@ngrx/store';
-
 
 const uri = realm.graphqlUrl;
 
@@ -32,8 +30,7 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
     BrowserModule,
 		AppRoutingModule,
     GraphQLModule,
-    HttpClientModule,
-    StoreModule.forRoot({}, {})
+    HttpClientModule
   ],
 	providers: [
 		{
