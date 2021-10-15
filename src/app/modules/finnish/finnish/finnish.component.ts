635bdafe5d1cacc1240bd9fae8c0b494f5bd7c33
Linus <vitocorleone77@gmail.com>
2021-10-15 23:07:20 +0200

    show subscription on finnish page

--- src/app/modules/finnish/finnish/finnish.component.ts
+++ src/app/modules/finnish/finnish/finnish.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Dictionary } from '@ngrx/entity';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Participant } from 'src/app/models/Participant';
+import { Subscriptor } from 'src/app/models/Subscriptor';
 
 import * as ParticipantReducer from '../../participant/state/participant.reducer';
+import * as SubscriptionReducer from '../../subscription/state/subscription.reducer'
 
 @Component({
   selector: 'app-finnish',
@@ -15,13 +17,18 @@ import * as ParticipantReducer from '../../participant/state/participant.reducer
 export class FinnishComponent implements OnInit {
 
   public title = 'Finnished';
-  public content: Observable<Dictionary<Participant>>;
+  public participants: Observable<Dictionary<Participant>>;
+  public subscription: Observable<Subscriptor>;
 
   constructor(
-    private store: Store<ParticipantReducer.State>) { }
+    private subscriptionStore: Store<SubscriptionReducer.State>,
+    private participantStore: Store<ParticipantReducer.State>) { }
 
   ngOnInit(): void {
-    this.content = this.store.select(ParticipantReducer.getAllParticipants).pipe(map(x => {
+    this.subscription = this.subscriptionStore.select(SubscriptionReducer.getSubscription).pipe(map(x => {
+      return x;
+    }));
+    this.participants = this.participantStore.select(ParticipantReducer.getAllParticipants).pipe(map(x => {
       return x;
     }));
   }
