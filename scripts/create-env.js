20de9ec87ed970c47987f72fc1da9c7c20521dcb
Linus <vitocorleone77@gmail.com>
2021-11-02 21:53:22 +0100

    APP_ID_REALM

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,2 +1,2 @@
 const fs = require('fs')
-fs.writeFileSync('./.env', `CLIENT_ROOT=${process.env.CLIENT_ROOT}\nMAX_NUMBER_OF_WEEKS=${process.env.MAX_NUMBER_OF_WEEKS}\nMAX_NUMBER_OF_RESERVATIONS=${process.env.MAX_NUMBER_OF_RESERVATIONS}\n`)
+fs.writeFileSync('./.env', `CLIENT_ROOT=${process.env.CLIENT_ROOT}\nMAX_NUMBER_OF_WEEKS=${process.env.MAX_NUMBER_OF_WEEKS}\nMAX_NUMBER_OF_RESERVATIONS=${process.env.MAX_NUMBER_OF_RESERVATIONS}\nAPP_ID_REALM=${process.env.APP_ID_REALM}\n`)
