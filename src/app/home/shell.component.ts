7c4a8f8785bd4ee90e0ad3f3db3e4311d1fad6b5
Don Corleone <vitocorleone77@gmail.com>
2022-03-26 17:41:02 +0100

    svg

--- src/app/home/shell.component.ts
+++ src/app/home/shell.component.ts
@@ -1,4 +1,10 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  OnInit,
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'pm-shell',
@@ -7,9 +13,38 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.Default,
 })
 export class ShellComponent implements OnInit {
-  constructor() {
+  constructor(@Inject(DOCUMENT) readonly document: Document) {}
+
+  /** The Window object from Document defaultView */
+  get window(): Window | null {
+    return this.document.defaultView;
   }
 
-  ngOnInit() {
+  routeTo(url: string) {
+    if (confirm(`Are you sure you want to leave and go to ${url}?`)) {
+      this.redirect(url);
+    }
+  }
+
+  ngOnInit(): void {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      const mySVG = document.getElementById('svg-icon');
+      mySVG?.setAttribute('viewBox', '830 0 3421.971 595.281');
+    }
+  }
+
+  /** Redirects to the specified external link with the mediation of the router */
+  private redirect(url: string, target = '_self'): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      try {
+        resolve(!!this.window?.open('https://' + url, target));
+      } catch (e) {
+        reject(e);
+      }
+    });
   }
 }
