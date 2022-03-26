315fee87ec1abc9d9e9f3e376a1ccf514bb35d40
Don Corleone <vitocorleone77@gmail.com>
2022-03-26 15:37:28 +0100

    logo added

--- src/app/home/shell.component.ts
+++ src/app/home/shell.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'pm-shell',
   templateUrl: './shell.component.html',
-  styleUrls: ['./shell.component.css'],
-  changeDetection: ChangeDetectionStrategy.Default
+  styleUrls: ['./shell.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class ShellComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
